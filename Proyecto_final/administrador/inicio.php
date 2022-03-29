7d7013dac2bd984231517ffc9315dcdf680fdfc7
Elizabeth <53582459+Elizabeth1994-lon@users.noreply.github.com>
2022-03-29 07:12:43 -0500

    plantilla creada para php y mysql

---
+++ Proyecto_final/administrador/inicio.php
@@ -0,0 +1,14 @@
+<?php include('plantilla/cabecera.php');  ?>
+          <div class="col-md-12">
+             <div class="jumbotron">
+                <h1 class="display-3">Bienvenido  </h1>
+                 <p class="lead">Vamos a administrar nuestros libros en el sitio web</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="seccion/productor.php" role="button">Adminitrar libros</a>
+                 </p>
+            </div>
+          </div>
+          
+<?php include('plantilla/pie.php');  ?>
---
+++ Proyecto_final/template/cabecera.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Viaje a otro mundo</title>
+
+<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+
+</head>
+<body>
+	
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+		<ul class="nav navbar-nav">
+			<li class="nav-item ">
+				<a class="nav-link" href="#">viaje a otra dimension </a>
+			</li>
+			
+			<li class="nav-item">
+				<a class="nav-link" href="index.php">Inicio</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="productos.php">Libros</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="nosotro.php">Nosotros</a>
+			</li>
+		</ul>
+	</nav>
+
+<div class="container">
+
+<br/> 
+
+	<div class="row">
